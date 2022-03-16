89607d8d9cab0a83a050a53c2864f346367b3b03
Quentin <quentin1998-007@hotmail.fr>
2022-03-16 15:41:44 +0100

    Structure de base de la home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './screens/home/home.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    TopBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
